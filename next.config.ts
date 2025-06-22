584b6219182cf9d4752ac3e0441188a1f28659fd
hassan14545 <anaada2015@gmail.com>
2025-06-22 15:18:10 +0600

    www

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,11 @@
 const nextConfig = {
   output: 'export', // 👈 required for GitHub Pages
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || "/my-portfolio", // Replace <repo-name> with your repo (e.g., "my-portfolio")
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || "/my-portfolio/",
   images: {
     unoptimized: true, // 👈 disable image optimization for static export
   },
-  basePath: "/portfolio", // 👈 if your repo is named 'portfolio'
+  // basePath: "/portfolio", // 👈 if your repo is named 'portfolio'
 };
 
 module.exports = nextConfig;
