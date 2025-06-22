845c3016491ebfc6fe69974248d446636d1b1a36
hassan14545 <anaada2015@gmail.com>
2025-06-22 20:08:30 +0600

    updated

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  basePath: '/my-portfolio', // your GitHub repo name
-  images: {
-    unoptimized: true, // required for static export
-  },
-  assetPrefix: '/my-portfolio/', // also based on repo name
+
 };
 
 module.exports = nextConfig;
